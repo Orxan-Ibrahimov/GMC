2aa00e9a4533649bb971d07aed6502d25f715939
Orxan-Ibrahimov <orxan.ibrahimli.98@gmail.com>
2024-08-27 23:36:24 +0400

    ContactPageContensFilled

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -6,11 +6,11 @@
 <div class="container-fluid page-header" style="margin-bottom: 90px;">
     <div class="container">
         <div class="d-flex flex-column justify-content-center" style="min-height: 300px">
-            <h3 class="display-4 text-white text-uppercase">Contact</h3>
+            <h3 class="display-4 text-white text-uppercase">Bizimlə əlaqə</h3>
             <div class="d-inline-flex text-white">
-                <p class="m-0 text-uppercase"><a class="text-white" href="/">Home</a></p>
+                <p class="m-0 text-uppercase"><a class="text-white" href="/">Ana Səhifə</a></p>
                 <i class="fa fa-angle-double-right pt-1 px-3"></i>
-                <p class="m-0 text-uppercase">Contact</p>
+                <p class="m-0 text-uppercase">Bizimlə əlaqə</p>
             </div>
         </div>
     </div>
@@ -22,8 +22,8 @@
 <div class="container-fluid py-5">
     <div class="container py-5">
         <div class="text-center mb-5">
-            <h5 class="text-info text-uppercase mb-3" style="letter-spacing: 5px;">Contact</h5>
-            <h1>Contact For Any Query</h1>
+            <h5 class="text-info text-uppercase mb-3" style="letter-spacing: 5px;">Bizimlə əlaqə</h5>
+            <h1>Sualın var?</h1>
         </div>
         <div class="row justify-content-center">
             <div class="col-lg-8">
@@ -31,23 +31,23 @@
                     <div id="success"></div>
                     <form name="sentMessage" id="contactForm" novalidate="novalidate">
                         <div class="control-group">
-                            <input type="text" class="form-control border-0 p-4" id="name" placeholder="Your Name" required="required" data-validation-required-message="Please enter your name" />
+                            <input type="text" class="form-control border-0 p-4" id="name" placeholder="Adınız" required="required" data-validation-required-message="Please enter your name" />
                             <p class="help-block text-danger"></p>
                         </div>
                         <div class="control-group">
-                            <input type="email" class="form-control border-0 p-4" id="email" placeholder="Your Email" required="required" data-validation-required-message="Please enter your email" />
+                            <input type="email" class="form-control border-0 p-4" id="email" placeholder="Soyadınız" required="required" data-validation-required-message="Please enter your email" />
                             <p class="help-block text-danger"></p>
                         </div>
                         <div class="control-group">
-                            <input type="text" class="form-control border-0 p-4" id="subject" placeholder="Subject" required="required" data-validation-required-message="Please enter a subject" />
+                            <input type="text" class="form-control border-0 p-4" id="subject" placeholder="Sual nə ilə bağlıdır?" required="required" data-validation-required-message="Please enter a subject" />
                             <p class="help-block text-danger"></p>
                         </div>
                         <div class="control-group">
-                            <textarea class="form-control border-0 py-3 px-4" rows="5" id="message" placeholder="Message" required="required" data-validation-required-message="Please enter your message"></textarea>
+                            <textarea class="form-control border-0 py-3 px-4" rows="5" id="message" placeholder="Mesajınız" required="required" data-validation-required-message="Please enter your message"></textarea>
                             <p class="help-block text-danger"></p>
                         </div>
                         <div class="text-center">
-                            <button class="btn btn-info py-3 px-5" type="submit" id="sendMessageButton">Send Message</button>
+                            <button class="btn btn-info py-3 px-5" type="submit" id="sendMessageButton">Göndər</button>
                         </div>
                     </form>
                 </div>
